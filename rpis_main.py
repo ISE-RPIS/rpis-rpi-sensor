fd3a06c23daa3ecf0124f8c019482ec96e9474f5
Youngman Jo <youngman7.jo@gmail.com>
2020-06-20 08:06:48 +0900

    Update main src
    1) Add input LED pin logic
    2) Improve program exit logic
    3) Improve detect logic; turn on LED step by step

--- rpis_main.py
+++ rpis_main.py
@@ -12,6 +12,10 @@ ULTRASOUNDS_DISTANCE = -1
 QUIT_BUTTON_PIN = -1
 PARKING_ID = -1
 MODE = -1
+LED1_PIN = -1
+LED2_PIN = -1
+LED3_PIN = -1
+LED4_PIN = -1
 
 print('[RPIS] Setting properties')
 
@@ -56,6 +60,22 @@ while True:
         break
 
 while True:
+    LED1_PIN = input('[RPIS] Set LED-1 pin : ')
+    LED2_PIN = input('[RPIS] Set LED-2 pin : ')
+    LED3_PIN = input('[RPIS] Set LED-3 pin : ')
+    LED4_PIN = input('[RPIS] Set LED-4 pin : ')
+    try:
+        LED1_PIN = int(LED1_PIN)
+        LED2_PIN = int(LED2_PIN)
+        LED3_PIN = int(LED3_PIN)
+        LED4_PIN = int(LED4_PIN)
+    except:
+        print('[RPIS] Occured exception! : Failed type casting "LED PIN"')
+        continue
+    if input('[RPIS] "{}, {}, {}, {}" is correct? (y/n) : '.format(LED1_PIN, LED2_PIN, LED3_PIN, LED4_PIN)).lower() == 'y':
+        break
+
+while True:
     PARKING_ID = input('[RPIS] Set "parking_id" : ')
     if input('[RPIS] "{0}" Is correct? (y/n) : '.format(PARKING_ID)).lower() == 'y':
         break
@@ -79,18 +99,28 @@ print('[RPIS] Activate RPIS main process...')
 AWS_IOT_ENDPOINT = 'a3d767kqnxh4m3-ats.iot.ap-northeast-2.amazonaws.com'
 AWS_IOT_PORT = 8883
 
+program_running = True
+def buttonOnClick(channel):
+    global program_running
+    program_running = False
+    print('[RPIS] Quit button pressed!')
+
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(ULTRASOUNDS_TRIG, GPIO.OUT)
 GPIO.setup(ULTRASOUNDS_ECHO, GPIO.IN)
 GPIO.setup(QUIT_BUTTON_PIN, GPIO.IN, GPIO.PUD_UP)
+GPIO.setup(LED1_PIN, GPIO.OUT)
+GPIO.setup(LED2_PIN, GPIO.OUT)
+GPIO.setup(LED3_PIN, GPIO.OUT)
+GPIO.setup(LED4_PIN, GPIO.OUT)
+GPIO.add_event_detect(QUIT_BUTTON_PIN, GPIO.FALLING, buttonOnClick)
+
+obj_detect_start = -1
 
 try:
-    while True:
-        if GPIO.input(QUIT_BUTTON_PIN) == False:
-            print('[RPIS] Quit button pressed!')
-            break
-
+    GPIO.output(LED1_PIN, True)
+    while program_running:
         # Loop time: 500ms + @
         GPIO.output(ULTRASOUNDS_TRIG, False)
         sleep(0.5)
@@ -107,7 +137,7 @@ try:
         distance = pulse_duration * 34000 / 2
         distance = round(distance, 2)
 
-        #print('[RPIS] Distance :', distance, 'cm')
+        # print('[RPIS] Distance :', distance, 'cm')
 
         if distance <= ULTRASOUNDS_DISTANCE:
             if obj_detect_start == -1:
@@ -116,16 +146,21 @@ try:
             # checking 2 sec
             if obj_detect_end - obj_detect_start >= 2:
                 print('[RPIS] 1. Detected!!!')
+                GPIO.output(LED2_PIN, True)
                 # Capture
                 cam = Camera()
                 cam.sensor_mode = 6
                 cam.rotation = 180
                 image = cam.capture_to_opencv()
+
                 print('[RPIS] 2. Capturing image is successfully!')
+                GPIO.output(LED3_PIN, True)
                 # Get license text
                 plate_char = LPR().get_license_plate_char(image)
                 print('[RPIS] 3. Getting license text is successfully!')
                 print('[RPIS] license : {0}'.format(plate_char))
+
+                GPIO.output(LED4_PIN, True)
                 # Send to AWS IoT using MQTT
                 data = {}
                 data['license'] = plate_char
@@ -134,7 +169,8 @@ try:
                     data['coming_time'] = str(time())
                 else:
                     data['outgoing_time'] = str(time())
-                client = MqttClient(endpoint=AWS_IOT_ENDPOINT, port=AWS_IOT_PORT, set_tls=True)
+                client = MqttClient(endpoint=AWS_IOT_ENDPOINT,
+                                    port=AWS_IOT_PORT, set_tls=True)
                 client.connect()
                 if MODE == 0:
                     client.publish('iot/rpis/coming', json.dumps(data))
@@ -145,9 +181,13 @@ try:
                 gc.collect()
         else:
             obj_detect_start = -1
+            GPIO.output(LED2_PIN, False)
+            GPIO.output(LED3_PIN, False)
+            GPIO.output(LED4_PIN, False)
 except:
     print('[RPIS] Error: Occured exception!!!')
 finally:
+    GPIO.output(LED1_PIN, False)
     GPIO.cleanup()
     print('[RPIS] GPIO cleanup successfully!')
 
